b6dde47bca4fea7342f3b5410ef3f3ef185a9521
unknown <asus@asus-PC.(none)>
2012-12-03 14:32:34 +0800

    third

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@
 /*
  * GET users listing.
  */
-
+var request = require('request'),
+$ = require('jQuery');
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
@@ -10,8 +11,16 @@ exports.list = function(req, res){
 // 	res.send("hello");
 // }
 exports.search=function(req,res,next){
-	console.log(req.query.keyword);
+	//console.log(req.query.keyword);
 	if(!req.query.keyword) throw new Error('no keyword');
 	//res.send(req.query.keyword);
-	res.render('search', {keyword : req.query.keyword});
+	var url="http://tulips.ntu.edu.tw/search*cht/X?SEARCH="+req.query.keyword+"&searchscope=5";
+	//console.log(url);
+	
+ 	request(url, function (error, response, body) {
+ 		var $body=$(body);
+ 		//console.log($body.find(".browseEntryData").text());
+ 		res.render('search', {keyword : req.query.keyword, data:$body.find(".briefcitTitleonly").text()});
+ 	});
+	
 	}
