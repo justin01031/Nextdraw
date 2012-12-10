23e773ca8650800c74d1ab490b6fb74601f02448
unknown <asus@asus-PC.(none)>
2012-12-10 16:55:32 +0800

    add background

--- public/javascripts/code.js
+++ public/javascripts/code.js
@@ -12,3 +12,37 @@ $('.animateMe').each(function(){
 				);
 		});
 $('#hide').css("opacity","0");;
+var speed = 0;
+var direction="top";
+var tab = document.getElementById("scrollbox");
+var tab1 = document.getElementById("boximg");
+var leftDir = document.getElementById("leftDir");
+var rightDir = document.getElementById("rightDir");
+function marquee(){
+    switch(direction){
+        case "top":
+           
+                tab.scrollTop+=100;
+           
+        break;
+        case "bottom":
+           
+                tab.scrollTop-=100;
+            
+        break;
+    }
+}
+function changeDirection(dir){
+   direction = dir;
+}
+// tab.onmouseover = function(){clearInterval(timer);};
+//tab.onmouseout = function(){timer = setInterval(marquee,speed);};
+leftDir.onclick = function(){
+  changeDirection("bottom");
+  marquee();
+  
+};
+rightDir.onclick = function(){
+  changeDirection("top");
+  marquee();
+};
